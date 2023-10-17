2545257a7c2a4a7eed14cd96ddd8a463d36f05af
mslysz <macslysz@gmail.com>
2023-10-17 06:55:02 +0100

    added 2nd exercise

--- main1.js
+++ main1.js
@@ -1,3 +1,4 @@
+//Exer 1
 //We need a function that can transform a number (integer) into a string.
 
 
@@ -8,3 +9,19 @@ function numberToString(num) {
 console.log(typeof (numberToString(123)));
 console.log(typeof (numberToString(999)));
 console.log(numberToString(-100));
+
+
+//Exer 2
+
+//Create a function which answers the question "Are you playing banjo?".
+//If your name starts with the letter "R" or lower case "r", you are playing banjo!
+//The function takes a name as its only argument, and returns one of the following strings:
+
+function areYouPlayingBanjo(name) {
+  return name.charAt(0) === 'R' || name.charAt(0) === 'r' ? `${name} plays banjo` : `${name} does not play banjo`;
+}
+
+console.log(areYouPlayingBanjo('Robert'));
+console.log(areYouPlayingBanjo('Martin'))
+console.log(areYouPlayingBanjo('Diana'))
+console.log(areYouPlayingBanjo('renata'))
