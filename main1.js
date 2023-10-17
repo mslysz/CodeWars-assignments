444a57b110e0c749ea900175793fc478f8b02088
mslysz <macslysz@gmail.com>
2023-10-17 05:30:45 +0100

    added cw exercise

---
+++ main1.js
@@ -0,0 +1,10 @@
+//We need a function that can transform a number (integer) into a string.
+
+
+function numberToString(num) {
+  return String(num);
+
+}
+console.log(typeof (numberToString(123)));
+console.log(typeof (numberToString(999)));
+console.log(numberToString(-100));
