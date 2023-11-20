ca3ef42d04f82a5faf5b72383063ecd9ee36f74d
mslysz <macslysz@gmail.com>
2023-11-20 06:30:52 +0000

    update

--- main2.js
+++ main2.js
@@ -163,3 +163,24 @@ console.log(
     'https://github.com/mslysz/CodeWars-assignments/blob/main/main2.js'
   )
 );
+
+//Exer 10
+//Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
+
+function persistence(num) {
+  let count = 0;
+
+  num = num.toString();
+  while (num.length > 1) {
+    count++;
+    num = num
+      .split('')
+      .map(Number)
+      .reduce((a, b) => a * b)
+      .toString();
+  }
+  return count;
+}
+
+console.log(persistence(39));
+console.log(persistence(999));
