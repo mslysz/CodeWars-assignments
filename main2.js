e37eeeceabf2a874ffe2bb6bed06ca774d0ac5c4
mslysz <macslysz@gmail.com>
2023-12-13 08:51:59 +0000

    added new exercise

--- main2.js
+++ main2.js
@@ -184,3 +184,29 @@ function persistence(num) {
 
 console.log(persistence(39));
 console.log(persistence(999));
+
+//Exer 11
+//Let's play! You have to return which player won! In case of a draw return Draw!.
+
+//Examples(Input1, Input2 --> Output):
+//"scissors", "paper" --> "Player 1 won!"
+//"scissors", "rock" --> "Player 2 won!"
+//"paper", "paper" --> "Draw!"
+
+const rps = (p1, p2) => {
+  if (
+    (p1 === 'scissors' && p2 === 'paper') ||
+    (p1 === 'rock' && p2 === 'scissors') ||
+    (p1 === 'paper' && p2 === 'rock')
+  ) {
+    return 'Player 1 won!';
+  } else if (p1 === p2) {
+    return 'Draw!';
+  } else {
+    return 'Player 2 won!';
+  }
+};
+
+console.log(rps('paper', 'scissors'));
+console.log(rps('scissors', 'rock'));
+console.log(rps('paper', 'paper'));
