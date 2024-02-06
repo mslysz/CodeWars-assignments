8b96831bef63debe41d6e3f051428c280d7f21ed
mslysz <macslysz@gmail.com>
2024-02-06 02:15:22 +0000

    added new exer

--- main3.js
+++ main3.js
@@ -17,3 +17,19 @@ function solution(string) {
 console.log(solution('camelCasing'));
 console.log(solution('camelCasingTest'));
 console.log(solution('upDownUpDownUpDown'));
+
+//Exer 2
+//Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0. Your function only needs to return the result.
+//2 -> 3 (1 + 2)
+//8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+
+const summation = (num) => {
+  let result = 0;
+  for (let i = 1; i <= num; i++) {
+    result += i;
+  }
+  return result;
+};
+
+console.log(summation(8));
+console.log(summation(2));
