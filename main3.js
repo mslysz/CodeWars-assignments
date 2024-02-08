5e29970bd99ae5404462b4753fed6a902a8c151b
mslysz <macslysz@gmail.com>
2024-02-08 08:14:16 +0000

    added new CW exercise

--- main3.js
+++ main3.js
@@ -100,3 +100,14 @@ console.log(likes(['Peter']));
 console.log(likes(['Jacob', 'Alex']));
 console.log(likes(['Max', 'John', 'Mark']));
 console.log(likes(['Max', 'John', 'Mark', 'Max']));
+
+//Write a function findNeedle() that takes an array full of junk but containing one "needle"
+//After your function finds the needle it should return a message (as a string) that says:
+//"found the needle at position " plus the index it found the needle, so:
+
+const findNeedle = (haystack) =>
+  'found the needle at position ' + haystack.indexOf('needle');
+
+console.log(
+  findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
+);
