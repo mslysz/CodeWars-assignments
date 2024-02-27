1f472ec70b8896924a24b0c3907636b00726a066
mslysz <macslysz@gmail.com>
2024-02-27 05:15:01 +0000

    new exercise

--- main3.js
+++ main3.js
@@ -123,3 +123,21 @@ String.prototype.isUpperCase = function () {
   }
   return true;
 };
+
+//Exer 8
+//Your function takes two arguments:
+//current father's age (years)
+//current age of his son (years)
+
+//Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  let result = dadYearsOld - sonYearsOld * 2;
+  if (result < 0) {
+    return result * -1;
+  }
+  return result;
+}
+
+console.log(twiceAsOld(36, 7));
+console.log(twiceAsOld(55, 30));
